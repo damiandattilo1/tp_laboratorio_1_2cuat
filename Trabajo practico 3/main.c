a816117038ccb087ab38e983d02d79a13ad089ae
damiandattilo1 <60588905+damiandattilo1@users.noreply.github.com>
2020-11-06 19:31:32 -0300

    Add files via upload

---
+++ Trabajo practico 3/main.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Employee.h"
+
+/****************************************************
+    Menu:
+     1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).
+     2. Cargar los datos de los empleados desde el archivo data.csv (modo binario).
+     3. Alta de empleado
+     4. Modificar datos de empleado
+     5. Baja de empleado
+     6. Listar empleados
+     7. Ordenar empleados
+     8. Guardar los datos de los empleados en el archivo data.csv (modo texto).
+     9. Guardar los datos de los empleados en el archivo data.csv (modo binario).
+    10. Salir
+*****************************************************/
+
+int menu();
+
+
+int main()
+{
+    int option=0;
+    LinkedList* listaEmpleados = ll_newLinkedList();
+
+    if(listaEmpleados==NULL)
+    {
+        printf("\nERROR No se pudo conseguir memoria\n\n");
+        exit(EXIT_FAILURE);
+    }
+
+    do
+    {
+        option=menu();
+        switch(option)
+        {
+            case 1:
+                if(!controller_loadFromText("data.csv",listaEmpleados))
+                {
+                    printf("\nERROR en la carga de datos\n\n");
+                }
+                else
+                {
+                    printf("\nArchivo cargado con exito\n\n");
+                }
+                break;
+            case 2:
+               // controller_loadFromBinary("dataB.bin",listaEmpleados);
+                break;
+            case 3:
+                system("cls");
+                if(!controller_addEmployee(listaEmpleados))
+                {
+                    printf("ERROR al cargar los datos\n");
+                }
+                else
+                {
+                    printf("\nCarga exitosa\n\n");
+                }
+                break;
+            case 4:
+                if(!controller_editEmployee(listaEmpleados))
+                {
+                    printf("\nNo se realizo ningun cambio\n\n");
+                }
+                else
+                {
+                    printf("\nCambio realizado con exito\n\n");
+                }
+                break;
+            case 5:
+                system("cls");
+                if(!controller_removeEmployee(listaEmpleados))
+                {
+                    printf("\nBaja exitosa\n\n");
+                }
+                break;
+            case 6:
+                if(!controller_ListEmployee(listaEmpleados))
+                {
+                    printf("\nERROR\n\n");
+                }
+                break;
+            case 7:
+                //controller_sortEmployee(listaEmpleados);
+                break;
+            case 8:
+                //controller_saveAsText("data.csv" , listaEmpleados);
+                break;
+            case 9:
+                //controller_saveAsBinary("dataB.bin" , listaEmpleados);
+                break;
+            case 10:
+                // Salida
+                break;
+            default:
+                system("cls");
+                printf("\nERROR, no es una opcion valida\n\n");
+                break;
+        }
+        system("pause");
+        system("cls");
+    }while(option != 10);
+    return 0;
+}
+
+
+
+
+int menu()
+{
+    int opcion;
+
+    printf("Menu:\n");
+    printf("\n1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).");
+    printf("\n2. Cargar los datos de los empleados desde el archivo data.bin (modo binario).");
+    printf("\n3. Alta de empleado");
+    printf("\n4. Modificar datos de empleado");
+    printf("\n5. Baja de empleado");
+    printf("\n6. Listar empleados");
+    printf("\n7. Ordenar empleados");
+    printf("\n8. Guardar los datos de los empleados en el archivo data.csv (modo texto).");
+    printf("\n9. Guardar los datos de los empleados en el archivo data.bin (modo binario).");
+    printf("\n10. Salir\n\n");
+
+    printf("Ingrese su opcion: ");
+    scanf("%d", &opcion);
+
+    return opcion;
+}
---
+++ Trabajo practico 3/parser.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "LinkedList.h"
+#include "Employee.h"
+
+#define BUFFER 1000
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+/*int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+
+    char* id[20];
+    char* nombre[128];
+    char* horasTrabajadas[20];
+    char* sueldo[20];
+    char entrada[4][100];
+    int total=0;
+    Employee* auxEmployee;
+    fscanf(pFile,"%*[^\n]\n");
+    while(!feof(pFile))
+    {
+        total=fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n]\n",entrada[0], entrada[1], entrada[2], entrada[3]);
+        if(total==4)
+        {
+            auxEmployee=employee_newParametros(entrada[0], entrada[1], entrada[2], entrada[3]);
+            ll_add(pArrayListEmployee, auxEmployee);
+        }
+    }
+    printf("\nSe leyeron %d entradas\n\n", total);
+
+
+    return 1;
+}*/
+
+int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+    int retorno = -1;
+    char bufferId[BUFFER];
+    char bufferName[BUFFER];
+    char bufferHorasTrabajadas[BUFFER];
+    char bufferSueldo[BUFFER];
+    Employee* pEmployee;
+    int total=0;
+    if(pFile != NULL)
+    {
+        while(!feof(pFile))
+        {
+            total+=fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n]\n",bufferId,bufferName,bufferHorasTrabajadas,bufferSueldo);
+            if(atoi(bufferSueldo) > 0)
+            {
+                pEmployee = employee_newParametros(bufferId,bufferName,bufferHorasTrabajadas,bufferSueldo);
+
+                if(pEmployee != NULL && atoi(bufferId) >= 0)
+                {
+                    ll_add(pArrayListEmployee,pEmployee);
+                    retorno = 0;
+                }
+            }
+        }
+        printf("\nSe cargaron %d datos\n\n", total);
+    }
+    return retorno;
+}
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+
+    return 1;
+}
